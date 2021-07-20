8703b4c230b56fd9d86533d018be6c640fb66a71
Yami98 <chibakenta123@gmail.com>
2021-07-20 21:27:45 +0900

    Adding status bot and activity

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const { Client } = require("discord.js");
 const { token } = require("./config");
-
+const alive = require("./../server")
 const client = new Client();
 
 require("./core/loadWidgetListeners")(client);
+
+client.on('ready', () => {
+  client.user.setPresence({
+    status: 'idle',
+    activity: {
+        name: 'My Citizen',
+        type: 'WATCHING',
+        url: 'https://www.twitch.tv/kouma98'
+    }
+  })
+});
 
 client.login(token).catch((error) => {
   console.error(error);
   process.exit(1);
 });
+
+
+
+alive()
